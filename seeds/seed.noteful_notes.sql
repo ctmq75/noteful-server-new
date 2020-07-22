343539fe210e27da4131ced8838926950a85c6e4
Chris McArdle <christopher.mcardle2@gmail.com>
2020-07-22 12:39:58 -0500

    various fixes

---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,5 @@
+INSERT INTO noteful_notes ( note_name, content, folder_id )
+VALUES 
+  ('Dog', 'contenttttttt', 1),
+  ('Cat', 'contenttt', 2),
+  ('Fish', 'fake content', 3);
---
+++ src/app.js
@@ -0,0 +1,38 @@
+
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config.js');
+const nRouter = require('./notes/notes-router.js');
+const fRouter = require('./folders/folders-router.js');
+
+
+const app = express();
+
+const morganSettings = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
+app.use(helmet());
+app.use(cors());
+
+app.use('/notes', nRouter);
+app.use('/folders', fRouter);
+
+app.get('/', (req, res) => {
+  res.send('Hello, noteful users!');
+});
+
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (NODE_ENV === 'production') {
+    console.error(error);
+     response = { error: { message: 'server error' }};
+  } else {
+    console.error(error);
+    response = { error, message: error.message };
+   }
+   res.status(500).json(response);
+ });
+
+module.exports = app;
